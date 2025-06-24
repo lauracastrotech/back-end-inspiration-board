37daf151291486df8634cdd584266d4893a8075a
Olya Mateshov <olyamateshov@gmail.com>
2025-06-24 16:51:05 -0400

    Updated Models

--- app/models/card.py
+++ app/models/card.py
@@ -1,7 +1,37 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
+from sqlalchemy import ForeignKey
+
+
+# avoids import errors for Board
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .board import Board
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
     likes_count: Mapped[int]
+
+    board_id: Mapped[int] = mapped_column(ForeignKey("board.board_id")) 
+    board: Mapped["Board"] = relationship(back_populates="card")
+    
+    def to_dict(self):
+        card_as_dict = {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
+        }
+
+        return card_as_dict
+    
+    @classmethod
+    def from_dict(cls, card_data):
+        new_card = Card(
+            card_id=card_data["card_id"],
+            message=card_data["message"],
+            likes_count=card_data["likes_count"],
+            board_id=card_data["board_id"]
+        )
+        return new_card
